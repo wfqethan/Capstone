46736f2f5a3b299adc44ff363951d5845f4eb8a2
Wesley Tatum <wesleyktatum@gmail.com>
2017-06-20 15:36:47 -0700

    Converted some ipynb's to .py functions

---
+++ functions/data_aggregator.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def data_aggregator(sampleName):
+    """
+    This function takes in a sample name and parses through the files containing cleaned .txt files pertaining to the
+    sample. It then combines the separate 2D scan arrays as a single 3D array. Finally, it writes this 3D array to a
+    new file.
+    
+    inputs: a string variable of the sample's identification
+    
+    outputs: the 3D array containing the aggregated data
+        *writes a new file in the directory corresponding to aggregated data
+    """
+    
+    adh = np.genfromtxt('../Data/AFM/CleanedTXT/Adhesion/%s.txt'% (sampleName))
+    defor = np.genfromtxt('../Data/AFM/CleanedTXT/Deformation/%s.txt'% (sampleName))
+    dis = np.genfromtxt('../Data/AFM/CleanedTXT/Dissipation/%s.txt'% (sampleName))
+    modul = np.genfromtxt('../Data/AFM/CleanedTXT/LogDMTModulus/%s.txt'% (sampleName))
+    stif = np.genfromtxt('../Data/AFM/CleanedTXT/Stiffness/%s.txt'% (sampleName))
+
+    x, y = adh.shape
+    z = 5
+    
+    aggr = np.ndarray([x, y, z])
+    
+    for i in range(x):
+        for j in range(y):
+            aggr[i, j, 0] = adh[i, j]
+
+    for i in range(x):
+        for j in range(y):
+            aggr[i, j, 1] = defor[i, j]
+
+    for i in range(x):
+        for j in range(y):
+            aggr[i, j, 2] = dis[i, j]
+
+    for i in range(x):
+        for j in range(y):
+            aggr[i, j, 3] = modul[i, j]
+
+    for i in range(x):
+        for j in range(y):
+            aggr[i, j, 4] = stif[i, j]
+            
+    %store aggr > '../Data/AFM/AggregatedData/%s.txt' % (sampleName)
+    
+    return aggr
